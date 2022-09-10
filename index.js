769e64be7ca3d0d7827660f3e571314fbdba146f
unknown <hamzarq999@gmail.com>
2022-09-10 05:55:23 -0700

    added js for notification and messages section

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const menu = document.querySelectorAll('.menu');
 
+// SIDEBAR 
+
 const changemenuactive = () => {
     menu.forEach(item  => {
         item.classList.remove('active');
@@ -12,9 +14,23 @@ menu.forEach(item => {
         item.classList.add('active');
         if (item.id == 'notifications'){
             document.querySelector('.notifications-popups').style.display = 'block';
+            document.querySelector('.notification-count').style.display = 'none';
         }
         else{
             document.querySelector('.notifications-popups').style.display = 'none';
         }
     })
+});
+
+// MESSAGES
+
+const messagessNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+
+messagessNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--primary-color)';
+    messagessNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout( () => {
+        messages.style.boxShadow = 'none'
+    }, 2000);
 });
